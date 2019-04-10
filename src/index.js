815c77ac1e61b66008a4f4adc059fe7320e24b17
Alex Berrios <alexalfonsob@gmail.com>
2019-04-10 17:49:44 +0000

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,79 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import react into the bundle
+import React from "react";
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+//include your index.scss file into the bundle
+import "./styless/index.scss";
+
+//import your own components
+import Nav from "./component/nav.jsx";
+import Header from "./component/header.jsx";
+import Card from "./component/card.jsx";
+import Footer from "./component/footer.jsx";
+
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<Nav
+					btnOne="Home"
+					btnTwo="About"
+					btnThree="Services"
+					btnFour="Contact"
+				/>
+				<div className="container">
+					<Header
+						title="Titulo Principal"
+						content="Contenido Principal"
+						btnName="Boton Principal"
+					/>
+					<div className="row text-center">
+						<div className="col-lg-3 col-md-6 mb-4">
+							<Card
+								img="http://placehold.it/500x325"
+								title="Titulo 1"
+								content="Contenido 1"
+								btnName="Boton 1"
+							/>
+						</div>
+						<div className="col-lg-3 col-md-6 mb-4">
+							<Card
+								img="http://placehold.it/500x325"
+								title="Titulo 2"
+								content="Contenido 2"
+								btnName="Boton 2"
+							/>
+						</div>
+						<div className="col-lg-3 col-md-6 mb-4">
+							<Card
+								img="http://placehold.it/500x325"
+								title="Titulo 3"
+								content="Contenido 3"
+								btnName="Boton 3"
+							/>
+						</div>
+						<div className="col-lg-3 col-md-6 mb-4">
+							<Card
+								img="http://placehold.it/500x325"
+								title="Titulo 4"
+								content="Contenido 4"
+								btnName="Boton 4"
+							/>
+						</div>
+					</div>
+				</div>
+
+				<footer className="py-5 bg-dark">
+					<Footer />
+				</footer>
+			</div>
+		);
+	}
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
